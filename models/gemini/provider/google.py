d23828377036fbadb729b688525098db882518ae
hjlarry <hjlarry@163.com>
2025-02-26 09:37:08 +0800

    fix gemini validate 404

--- models/gemini/provider/google.py
+++ models/gemini/provider/google.py
@@ -17,7 +17,7 @@ class GoogleProvider(ModelProvider):
         """
         try:
             model_instance = self.get_model_instance(ModelType.LLM)
-            model_instance.validate_credentials(model="gemini-pro", credentials=credentials)
+            model_instance.validate_credentials(model="gemini-2.0-flash", credentials=credentials)
         except CredentialsValidateFailedError as ex:
             raise ex
         except Exception as ex:
