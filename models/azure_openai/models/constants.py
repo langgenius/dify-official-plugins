5e977880cb7ed8fbd8e3cf079946c3c9e9f47bd4
kurokobo <2920259+kurokobo@users.noreply.github.com>
2025-03-09 13:45:07 +0000

    feat: add o1 model

--- models/azure_openai/models/constants.py
+++ models/azure_openai/models/constants.py
@@ -1279,6 +1279,57 @@ LLM_BASE_MODELS = [
         ),
     ),
     AzureBaseModel(
+        base_model_name="o1",
+        entity=AIModelEntity(
+            model="fake-deployment-name",
+            label=I18nObject(
+                en_US="fake-deployment-name-label",
+            ),
+            model_type=ModelType.LLM,
+            features=[
+                ModelFeature.AGENT_THOUGHT,
+                ModelFeature.VISION,
+                ModelFeature.MULTI_TOOL_CALL,
+                ModelFeature.STREAM_TOOL_CALL,
+            ],
+            fetch_from=FetchFrom.CUSTOMIZABLE_MODEL,
+            model_properties={
+                ModelPropertyKey.MODE: LLMMode.CHAT.value,
+                ModelPropertyKey.CONTEXT_SIZE: 200000,
+            },
+            parameter_rules=[
+                ParameterRule(
+                    name="response_format",
+                    label=I18nObject(zh_Hans="回复格式", en_US="response_format"),
+                    type="string",
+                    help=I18nObject(
+                        zh_Hans="指定模型必须输出的格式",
+                        en_US="specifying the format that the model must output",
+                    ),
+                    required=False,
+                    options=["text", "json_object", "json_schema"],
+                ),
+                ParameterRule(
+                    name="json_schema",
+                    label=I18nObject(en_US="JSON Schema"),
+                    type="text",
+                    help=I18nObject(
+                        zh_Hans="设置返回的json schema，llm将按照它返回",
+                        en_US="Set a response json schema will ensure LLM to adhere it.",
+                    ),
+                    required=False,
+                ),
+                _get_o1_max_tokens(default=512, min_val=1, max_val=100000),
+            ],
+            pricing=PriceConfig(
+                input=15.00,
+                output=60.00,
+                unit=0.000001,
+                currency="USD",
+            ),
+        ),
+    ),
+    AzureBaseModel(
         base_model_name="o3-mini",
         entity=AIModelEntity(
             model="fake-deployment-name",
