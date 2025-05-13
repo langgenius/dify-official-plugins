c672d54ceae59bf12b4b1a491727eaec760846ca
kocomic <sqzhuo@gmail.com>
2025-05-13 12:31:26 +0800

    feat: (FishAudio)Added support for additional audio parameters.

--- tools/fishaudio/tools/fishaudio.py
+++ tools/fishaudio/tools/fishaudio.py
@@ -1,7 +1,7 @@
 from typing import Generator, Optional, Annotated, AsyncGenerator, Literal
 import httpx
 import ormsgpack
-from pydantic import AfterValidator, BaseModel, conint
+from pydantic import AfterValidator, BaseModel, conint, confloat
 from dify_plugin.errors.model import InvokeBadRequestError
 
 class ServeReferenceAudio(BaseModel):
@@ -20,29 +20,43 @@ class TTSRequest(BaseModel):
     # For example, if you want use https://fish.audio/m/7f92f8afb8ec43bf81429cc1c9199cb1/
     # Just pass 7f92f8afb8ec43bf81429cc1c9199cb1
     reference_id: str | None = None
+    prosody: dict | None = None
     # Normalize text for en & zh, this increase stability for numbers
     normalize: bool = True
     # Balance mode will reduce latency to 300ms, but may decrease stability
     latency: Literal["normal", "balanced"] = "normal"
 
 class FishAudio():
-    def __init__(self, url_base, api_key):
+    def __init__(self, url_base, api_key, tts_model = None):
         self.url_base = url_base
         self.api_key = api_key
+        self.tts_model = tts_model
         if not api_key:
             raise InvokeBadRequestError("API key not found")
 
     def headers(self):
-        return {
-            "authorization": f"Bearer {self.api_key}",
-            "content-type": "application/msgpack",
-        }
+        if not self.tts_model:
+            return {
+                "authorization": f"Bearer {self.api_key}",
+                "content-type": "application/msgpack",
+            }
+        else:
+            return {
+                "authorization": f"Bearer {self.api_key}",
+                "content-type": "application/msgpack",
+                "model": self.tts_model,
+            }
 
-    def tts(self, content, voice, latency, audio_format) -> Generator[bytes, None, None]:        
+    def tts(self, content, voice, latency, audio_format, audio_speed: Annotated[float, confloat(ge=0.5, le=2.0, strict=True)] = 1, chunk_length: Annotated[int, conint(ge=100, le=300, strict=True)] = 200, normalize: bool = True) -> Generator[bytes, None, None]:
         request = TTSRequest(
             text=content,
             format=audio_format,
             reference_id=voice,
+            prosody= {
+                "speed": audio_speed
+            },
+            chunk_length=chunk_length,
+            normalize=normalize,
             latency=latency,
         )
         try:
