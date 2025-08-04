190c7943dc2ef56801430c5ad391063234c6759f
zhengweijun <weijun.zheng@aminer.cn>
2025-08-04 16:46:27 +0800

    add 4.1v image support

--- models/zhipuai/models/llm/llm.py
+++ models/zhipuai/models/llm/llm.py
@@ -152,7 +152,7 @@ class ZhipuAILargeLanguageModel(_CommonZhipuaiAI, LargeLanguageModel):
                 PromptMessageRole.TOOL,
             }:
                 if isinstance(copy_prompt_message.content, list):
-                    if model not in {"glm-4v", "glm-4v-plus", "glm-4v-flash","glm-4.1v-thinking-flash","glm-4.1v-thinking-flashx"}:
+                    if model not in {"glm-4v", "glm-4v-plus", "glm-4v-flash", "glm-4.1v-thinking-flash", "glm-4.1v-thinking-flashx"}:
                         continue
                     if not isinstance(copy_prompt_message, UserPromptMessage):
                         continue
@@ -223,7 +223,7 @@ class ZhipuAILargeLanguageModel(_CommonZhipuaiAI, LargeLanguageModel):
         elif "json_schema" in model_parameters:
             del model_parameters["json_schema"]
 
-        if model in {"glm-4v", "glm-4v-plus", "glm-4v-flash","glm-4.1v-thinking-flash","glm-4.1v-thinking-flashx"}:
+        if model in {"glm-4v", "glm-4v-plus", "glm-4v-flash", "glm-4.1v-thinking-flash", "glm-4.1v-thinking-flashx"}:
             params = self._construct_glm_4v_parameter(
                 model, new_prompt_messages, model_parameters
             )
@@ -348,9 +348,6 @@ class ZhipuAILargeLanguageModel(_CommonZhipuaiAI, LargeLanguageModel):
         """
         text = ""
         assistant_tool_calls: list[AssistantPromptMessage.ToolCall] = []
-        # if model in {"glm-4.1v-thinking-flash"}:
-        #     for choice in response.choices:
-        #         text += choice.message.reasoning_content or ""
         for choice in response.choices:
             if choice.message.tool_calls:
                 for tool_call in choice.message.tool_calls:
