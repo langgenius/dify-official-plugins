c6bc1a0e9cfcac65dfb76e1e25caeb3f4097a6e7
Yuichiro Utsumi <utsumi.yuichiro@fujitsu.com>
2025-11-14 07:36:20 +0000

    feat(openai_api_compatible): text embedding prefix
    Allow configuration of document and query prefixes for text embedding.
    
    Signed-off-by: Yuichiro Utsumi <utsumi.yuichiro@fujitsu.com>

--- models/openai_api_compatible/models/text_embedding/text_embedding.py
+++ models/openai_api_compatible/models/text_embedding/text_embedding.py
@@ -1,6 +1,7 @@
-from typing import Mapping
+from typing import Mapping, Optional
 
-from dify_plugin.entities.model import AIModelEntity, I18nObject
+from dify_plugin.entities.model import AIModelEntity, EmbeddingInputType, I18nObject
+from dify_plugin.entities.model.text_embedding import TextEmbeddingResult
 
 from dify_plugin.interfaces.model.openai_compatible.text_embedding import OAICompatEmbeddingModel
 
@@ -19,3 +20,39 @@ class OpenAITextEmbeddingModel(OAICompatEmbeddingModel):
             )
 
         return entity
+
+    def _invoke(
+        self,
+        model: str,
+        credentials: dict,
+        texts: list[str],
+        user: Optional[str] = None,
+        input_type: EmbeddingInputType = EmbeddingInputType.DOCUMENT,
+    ) -> TextEmbeddingResult:
+        """
+        Invoke text embedding model
+
+        :param model: model name
+        :param credentials: model credentials
+        :param texts: texts to embed
+        :param user: unique user id
+        :param input_type: input type
+        :return: embeddings result
+        """
+
+        prefix = self._get_prefix(credentials, input_type)
+        texts = self._add_prefix(texts, prefix)
+
+        return super()._invoke(model, credentials, texts, user)
+
+    def _get_prefix(self, credentials: dict, input_type: EmbeddingInputType) -> str:
+        if input_type == EmbeddingInputType.DOCUMENT:
+            return credentials.get("document_prefix", "")
+
+        if input_type == EmbeddingInputType.QUERY:
+            return credentials.get("query_prefix", "")
+
+        return ""
+
+    def _add_prefix(self, texts: list[str], prefix: str) -> list[str]:
+        return [f"{prefix} {text}" for text in texts]
