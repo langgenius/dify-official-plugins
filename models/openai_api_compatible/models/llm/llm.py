ad02300c40516dff2070600d2edc409d86965701
tianzixuan335 <tianzixuan335@hellobike.com>
2025-11-29 16:38:41 +0800

    feat: add new thinking feature to support deepseek(from #2151)
    Signed-off-by: tianzixuan335 <tianzixuan335@hellobike.com>

--- models/openai_api_compatible/models/llm/llm.py
+++ models/openai_api_compatible/models/llm/llm.py
@@ -188,6 +188,8 @@ class OpenAILargeLanguageModel(OAICompatLargeLanguageModel):
                 
         if enable_thinking_value is not None:
             model_parameters.setdefault("chat_template_kwargs", {})["enable_thinking"] = enable_thinking_value
+            # Add From: https://github.com/langgenius/dify-official-plugins/pull/2151
+            model_parameters.setdefault("chat_template_kwargs", {})["thinking"] = enable_thinking_value
         
         # Remove thinking content from assistant messages for better performance.
         with suppress(Exception):
