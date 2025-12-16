f5ed53200f35107423e84b25ca058caf06c965b0
sai <>
2025-12-16 22:57:52 +0800

    Use `embed_multimodal_documents` instead when the embedding model is a vision model.

--- models/tongyi/models/text_embedding/text_embedding.py
+++ models/tongyi/models/text_embedding/text_embedding.py
@@ -1,4 +1,6 @@
 import time
+import os
+import yaml
 from typing import Optional
 import dashscope
 import numpy as np
@@ -101,6 +103,13 @@ class TongyiTextEmbeddingModel(_CommonTongyi, TextEmbeddingModel):
         Returns:
             List of embeddings, one for each text, and tokens usage.
         """
+
+        # If the model is vision model, it has different endpoint
+        # transfer and call embed_multimodal_documents
+        if TongyiTextEmbeddingModel._is_vision_model(model):
+            documents = [MultiModalContent(content_type=MultiModalContentType.TEXT, content=text) for text in texts]
+            return TongyiTextEmbeddingModel.embed_multimodal_documents(credentials_kwargs, model, documents)
+
         embeddings = []
         embedding_used_tokens = 0
         
@@ -144,6 +153,31 @@ class TongyiTextEmbeddingModel(_CommonTongyi, TextEmbeddingModel):
                 raise ValueError(f"Response usage is missing or does not contain total tokens: {response}")
                 
         return ([list(map(float, e)) for e in embeddings], embedding_used_tokens)
+
+    @staticmethod
+    def _is_vision_model(model: str) -> bool:
+        """
+        Check whether there is a YAML configuration file in the current directory and whether it includes vision features.
+
+        Args:
+            model: The model name
+        """
+        try:
+            current_dir = os.path.dirname(os.path.abspath(__file__))
+            yaml_file_path = os.path.join(current_dir, f"{model}.yaml")
+
+            if os.path.exists(yaml_file_path):
+                with open(yaml_file_path, 'r', encoding='utf-8') as f:
+                    yaml_content = yaml.safe_load(f)
+
+                if (yaml_content and
+                        'features' in yaml_content and
+                        isinstance(yaml_content['features'], list) and
+                        'vision' in yaml_content['features']):
+                    return True
+        except:
+            pass
+        return False
 
     def _calc_response_usage(self, model: str, credentials: dict, tokens: int) -> EmbeddingUsage:
         """
