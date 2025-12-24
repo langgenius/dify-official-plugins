b72b774d533ec20c0cb41542078053debbcd0391
zhaojiangang-it <119225398+zhaojiangang-it@users.noreply.github.com>
2025-12-24 09:58:59 +0800

    feat: update tongyi model bury point header, update version (#2315)
    * feat: update tongyi model bury point header, update version
    
    * feat: update tongyi model bury point header, update version
    
    ---------
    
    Co-authored-by: zhaojiangang-it <zjg02419448@alibaba-inc.com>

--- models/tongyi/models/llm/llm.py
+++ models/tongyi/models/llm/llm.py
@@ -922,15 +922,10 @@ class TongyiLargeLanguageModel(LargeLanguageModel):
                     if len(system_entry_split) >= 2:
                         burn = system_entry_split[0].split(',')
                         bury_point_header = json.loads(BURY_POINT_HEADER.get('x-dashscope-euid'))
-                        if len(burn) == 1:
+                        if len(burn) in (1, 2):
                             product_code = burn[0]
-                            buyer_uid = ""
-                            bury_point_header['moduleCode'] = f'market_{product_code}'.strip()
-                            bury_point_header['accountId'] = buyer_uid
-                        if len(burn) == 2:
-                            product_code = burn[0]
-                            buyer_uid = burn[1]
-                            bury_point_header['moduleCode'] = f'market_{product_code}'.strip()
+                            buyer_uid = burn[1] if len(burn) == 2 else ""
+                            bury_point_header['moduleCode'] = product_code.strip()
                             bury_point_header['accountId'] = buyer_uid.strip()
 
                         system_entries[0]['content'] = "".join(system_entry_split[1:])
