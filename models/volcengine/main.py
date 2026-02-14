67cbc235f6603b58dec9cb58423dea4e479e242a
Stream <Stream_2@qq.com>
2026-02-15 00:21:47 +0800

    fix: align with anthropic

--- models/volcengine/main.py
+++ models/volcengine/main.py
@@ -1,6 +1,9 @@
-from dify_plugin import DifyPluginEnv, Plugin
+from dify_plugin import Plugin, DifyPluginEnv
+import logging
+
+logging.basicConfig(level=logging.INFO)
 
 plugin = Plugin(DifyPluginEnv())
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     plugin.run()
