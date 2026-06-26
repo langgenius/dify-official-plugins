d75f5159096706c0dd732c4c357e371906a28262
fatelei <fatelei@gmail.com>
2026-06-26 10:33:10 +0800

    fix: fix confluence auth failed

--- tools/confluence/tools/auth.py
+++ tools/confluence/tools/auth.py
@@ -7,11 +7,10 @@ def auth(credential: dict[str, Any]) -> Confluence:
     """
     Authenticate to Confluence using environment variables.
     """
-    token_type = credential.get('token_type', 'Bearer')
     confluence = Confluence(
         url=credential.get("url"),
-        header={
-            "Authorization": f'{token_type} {credential.get("token")}'
-        }
+        username=credential.get("username"),
+        password=credential.get("token"),
     )
     return confluence
+
